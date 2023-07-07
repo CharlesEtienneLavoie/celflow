1826c76d7c9838837aa821e34797bba4849d35d9
CharlesEtienneLavoie <celavoie96@gmail.com>
2023-07-07 12:14:29 -0400

    update clean_attention function

--- R/clean_attention.R
+++ R/clean_attention.R
@@ -7,17 +7,17 @@
 #' the total number of correct responses per participant and removes participants
 #' who do not reach a specified minimum number of correct answers.
 #'
-#' @param data A dataset where attention check failures need to be identified and removed.
-#' @param questions A vector of column names in the dataset that represent the questions
+#' @param data A dataframe where attention check failures need to be identified and removed.
+#' @param questions A character vector of column names in the dataframe that represent the questions
 #' used for attention checks.
-#' @param correct_answers A vector of the correct answers to the attention check
+#' @param correct_answers A numeric vector of the correct answers to the attention check
 #' questions. Must be the same length as the `questions` vector.
 #' @param min_correct The minimum number of correct answers a participant must
-#' have to be retained in the dataset (default is 1).
-#' @param set_missing_to_zero A logical value to decide if missing responses should
+#' have to be retained in the dataframe (default is 1).
+#' @param set_missing_to_zero A logical value indicating whether missing responses should
 #' be treated as incorrect (default is FALSE).
 #'
-#' @return A dataset where responses from participants who failed the attention
+#' @return A dataframe where responses from participants who failed the attention
 #' checks have been removed.
 #' @references Remember to add reference here.
 #' @export
@@ -37,47 +37,59 @@
 #' clean_data <- clean_attention(df, questions = c("q1", "q2", "q3"),
 #' correct_answers = correct_answers, min_correct = 2, set_missing_to_zero = TRUE)
 #' }
+clean_attention <- function (data, questions, correct_answers, min_correct = 1, set_missing_to_zero = FALSE) {
 
-
-
-clean_attention <- function(data, questions, correct_answers, min_correct = 1, set_missing_to_zero = FALSE) {
-  # Check that the correct answers vector has the same length as the questions vector
+  # Ensure that the length of questions and correct_answers are the same
   if (length(questions) != length(correct_answers)) {
     stop("The length of the questions vector and the correct answers vector must be the same.")
   }
 
+  # Loop over each question
   for (i in 1:length(questions)) {
-    # Initialize a new column for each question to store whether the answer is correct
+
+    # Initialize a new column for each question's correct answer
     data[paste0("correct_answer", i)] <- numeric(nrow(data))
 
-    # Count the number of NAs for each question
+    # Calculate the number of missing responses for the current question
     num_na <- sum(is.na(data[[questions[i]]]))
     cat("Number of participants with NA for ", questions[i], ": ", num_na, "\n")
 
-    # For each row in the data, check if the answer is correct
+    # Loop over each row (participant) in the data
     for (j in 1:nrow(data)) {
-      # Check if the answer is missing and if missing answers should be treated as incorrect
-      if (is.na(data[j, questions[i]]) && set_missing_to_zero) {
-        data[j, paste0("correct_answer", i)] <- 0
-      } else if (data[j, questions[i]] == correct_answers[i]) {  # Check if the answer is correct
+
+      # Check if the current response is missing
+      if (is.na(data[j, questions[i]])) {
+        if (set_missing_to_zero) {
+          data[j, paste0("correct_answer", i)] <- 0
+        }
+
+        # Check if the current response is correct
+      } else if (data[j, questions[i]] == correct_answers[i]) {
         data[j, paste0("correct_answer", i)] <- 1
-      } else {  # The answer is incorrect
+
+        # The current response is incorrect
+      } else {
         data[j, paste0("correct_answer", i)] <- 0
       }
     }
   }
 
   # Calculate the total number of correct answers for each participant
-  data$correct_answer_total <- rowSums(data[, sapply(1:length(questions), function(x) paste0("correct_answer", x))])
+  if(length(questions) > 1) {
+    data$correct_answer_total <- rowSums(data[, sapply(1:length(questions), function(x) paste0("correct_answer", x))])
+  } else {
+    data$correct_answer_total <- data[, sapply(1:length(questions), function(x) paste0("correct_answer", x))]
+  }
 
-  # Print the number of participants that will be excluded based on the min_correct criterion
+  # Calculate the number of initial and remaining participants
   num_initial_participants <- nrow(data)
   num_remaining_participants <- nrow(data[data$correct_answer_total >= min_correct, ])
+
+  # Print the number of participants excluded due to failing attention checks
   cat("Number of participants excluded due to failing attention checks: ", num_initial_participants - num_remaining_participants, "\n")
 
-  # Exclude participants that do not meet the min_correct criterion
+  # Exclude participants who failed the attention checks
   data <- data[data$correct_answer_total >= min_correct, ]
 
   return(data)
 }
-
