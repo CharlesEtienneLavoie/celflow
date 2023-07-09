b7527af01b843c6bc7de89c9bed9f8dbf7c56f83
CharlesEtienneLavoie <celavoie96@gmail.com>
2023-07-09 14:23:18 -0400

    process_qualtrics_files optimization

--- R/process_qualtrics_files.R
+++ R/process_qualtrics_files.R
@@ -25,15 +25,14 @@
 
 
 
-# Function to process files
 process_qualtrics_files <- function(data_files, rename_files = FALSE, new_names = NULL) {
   # If rename_files is FALSE or not provided, use the original file names
   if (!rename_files) {
-    new_names <- gsub("\\.csv$", "", basename(data_files))
+    new_names <- tolower(gsub("\\.csv$", "", basename(data_files)))
   }
   # If rename_files is TRUE but no new_names list is provided, create new names
   else if (is.null(new_names)) {
-    new_names <- gsub(".+\\+-+(.+?)_.+", "\\1", basename(data_files))
+    new_names <- tolower(gsub(".+\\+-+(.+?)_.+", "\\1", basename(data_files)))
   }
 
   for (i in seq_along(data_files)) {
@@ -46,56 +45,17 @@ process_qualtrics_files <- function(data_files, rename_files = FALSE, new_names 
     cleaned_data <- data[-(1:2),]
 
     # Save and reload the cleaned data and labels directly within the loop
-    filename_data <- paste0('data_', gsub('\\W', '', new_name), '.csv')
-    filename_labels <- paste0('labels_data_', gsub('\\W', '', new_name), '.csv')
+    filename_data <- paste0('data_', gsub('\\W', '', tolower(new_name)), '.csv')
+    filename_labels <- paste0('labels_data_', gsub('\\W', '', tolower(new_name)), '.csv')
 
-    write.csv(cleaned_data, filename_data, na = "")# Function to process files
-    process_qualtrics_files <- function(data_files, rename_files = FALSE, new_names = NULL) {
-      # If rename_files is FALSE or not provided, use the original file names
-      if (!rename_files) {
-        new_names <- gsub("\\.csv$", "", basename(data_files))
-      }
-      # If rename_files is TRUE but no new_names list is provided, create new names
-      else if (is.null(new_names)) {
-        new_names <- gsub(".+\\+-+(.+?)_.+", "\\1", basename(data_files))
-      }
-
-      for (i in seq_along(data_files)) {
-        file <- data_files[i]
-        new_name <- new_names[i]
-        data <- read.csv(file, na.strings = c("", "NA", "N/A"), header = TRUE)
-
-        # Extract labels and clean data directly within the loop
-        labels <- data[1, ]
-        cleaned_data <- data[-(1:2),]
-
-        # Save and reload the cleaned data and labels directly within the loop
-        filename_data <- paste0('data_', gsub('\\W', '', new_name), '.csv')
-        filename_labels <- paste0('labels_data_', gsub('\\W', '', new_name), '.csv')
-
-        write.csv(cleaned_data, filename_data, na = "")
-        write.csv(labels, filename_labels, na = "")
-
-        cleaned_data <- read.csv(filename_data, na.strings = c("", "NA", "N/A"), header = TRUE)
-        labels <- read.csv(filename_labels, na.strings = c("", "NA", "N/A"), header = TRUE)
-
-        # Assign the cleaned data and labels to new objects in the global environment
-        assign(paste0('data_', gsub('\\W', '', new_name)), cleaned_data, envir = .GlobalEnv)
-        assign(paste0('labels_data_', gsub('\\W', '', new_name)), labels, envir = .GlobalEnv)
-      }
-    }
-
+    write.csv(cleaned_data, filename_data, na = "")
     write.csv(labels, filename_labels, na = "")
 
     cleaned_data <- read.csv(filename_data, na.strings = c("", "NA", "N/A"), header = TRUE)
     labels <- read.csv(filename_labels, na.strings = c("", "NA", "N/A"), header = TRUE)
 
     # Assign the cleaned data and labels to new objects in the global environment
-    assign(paste0('data_', gsub('\\W', '', new_name)), cleaned_data, envir = .GlobalEnv)
-    assign(paste0('labels_data_', gsub('\\W', '', new_name)), labels, envir = .GlobalEnv)
+    assign(paste0('data_', gsub('\\W', '', tolower(new_name))), cleaned_data, envir = .GlobalEnv)
+    assign(paste0('labels_data_', gsub('\\W', '', tolower(new_name))), labels, envir = .GlobalEnv)
   }
 }
-
-
-
-
